d7492c216b8855822c99c1bf81e847977959d55e
brunofluna <bruno@brunoluna.com.br>
2024-10-09 17:24:05 -0300

    update 1.1

--- main_app.py
+++ main_app.py
@@ -65,6 +65,65 @@ def captura(largura, altura):
     camera.release()
     cv2.destroyAllWindows()
     # fim da função
+def get_imagem_com_id():
+    caminhos = [os.path.join("fotos", f) for f in os.listdir("fotos")]
+    faces = []
+    ids = []
+
+    for caminho_imagem in caminhos:
+        imagem_face = cv2.cvtColor(cv2.imread(caminho_imagem), cv2.COLOR_BGR2GRAY)
+        id = int(os.path.split(caminho_imagem)[-1].split('.')[1])
+        ids.append(id)
+        faces.append(imagem_face)
+
+    return np.array(ids), faces
+
+def treinamento():
+    #cria os elementos de reconhecimento necessário
+    eigenface = cv2.face.EigenFaceRecognizer_create()
+    fisherface = cv2.face.FisherFaceRecognizer_create()
+    lbph = cv2.face.LBPHFaceRecognizer_create()
+
+    ids, faces = get_imagem_com_id()
+
+    #trinando o algoritmo do programa
+    print("Treinando... ")
+    eigenface.train(faces, ids)    
+    eigenface.write("classificadorEigen.yml")
+    fisherface.train(faces, ids)    
+    fisherface.write("classificadorFisher.yml")
+    lbph.train(faces, ids)
+    lbph.write("classificadorLBPH.yml")
+
+    #finaliza o trinamento
+    print("Treinamento finalizado com sucesso!")
+
+def reconhecedor_eigenfaces(largura, altura):
+    detector_faces = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    reconhecedor = cv2.face.EigenFaceRecognizer_create()
+    reconhecedor.read("classificadorEigen.yml")
+    fonte = cv2.FONT_HERSHEY_COMPLEX_SMALL
+
+    camera = cv2.VideoCapture(0)
+    while True:
+        conectado, imagem = camera.read()
+        imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+        faces_detectadas = detector_faces.detectMultiScale(imagem_cinza, scaleFactor=1.5, minSize=(30,30))
+
+        for (x, y, l, a) in faces_detectadas:
+            imagem_face = cv2.resize(imagem_cinza[y:y + a, x:x + l], (largura, altura))
+            cv2.rectangle(imagem, (x,y), (x+l, y+a), (0,0,255), 2)
+            id, confianca = reconhecedor.predict(imagem_face)
+            cv2.putText(imagem, str(id), (x,y+(a+30)), fonte, 2, (0,0,255))
+
+        #encerra a camera
+        cv2.imshow('Reconhecer faces', imagem)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
+
 
 # programa principal
 if __name__ == "__main__":
@@ -76,6 +135,8 @@ if __name__ == "__main__":
         # menu
         print('0 - Sair do programa.')
         print('1 - Capturar imagem do usuário.')
+        print('2 - Treinar sistema.')
+        print('3 - Detecção de faces.')
 
         opcao = input('Opção desejada: ')
         match opcao:
@@ -84,4 +145,13 @@ if __name__ == "__main__":
                 break
             case '1':
                 captura(largura, altura)
+                continue
+            case '2':
+                treinamento()
+                continue
+            case '3':
+                reconhecedor_eigenfaces(largura, altura)
+                continue 
+            case _:
+                print("Opção Inválida!")
                 continue
